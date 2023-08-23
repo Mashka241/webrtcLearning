ccc24c7d2cea093c1fe9b38679ab34d58b33117d
Maria Ivanova <mariaivanova@microsoft.com>
2023-08-23 23:15:01 +0200

    add peer connections

--- index.js
+++ index.js
@@ -1,19 +1,40 @@
 (async () => {
-    const mediaStream = await navigator.mediaDevices.getUserMedia({ video: { width: 360, height: 640 } });
-    window.mediaStream = mediaStream;
-    console.log('settings 1', mediaStream.getVideoTracks()[0].getSettings(), mediaStream.getVideoTracks().length);
-    const videoEl = document.createElement('video');
-    videoEl.srcObject = mediaStream;
-    videoEl.setAttribute('autoplay', true);
+    const constraints = { video: { width: 360, height: 640 } };
+    const localStream = await navigator.mediaDevices.getUserMedia(constraints);
+    const localVideo = document.createElement('video');
+    localVideo.setAttribute('id', 'localVideo');
+    localVideo.setAttribute('autoplay', true);
+    localVideo.srcObject = localStream;
     const body = document.body;
-    body.appendChild(videoEl)
+    body.appendChild(localVideo);
 
-    mediaStream.getVideoTracks()[0].addEventListener('mute', () => { console.log('mute') });
-    mediaStream.getVideoTracks()[0].addEventListener('unmute', () => { console.log('unmute') });
-    mediaStream.getVideoTracks()[0].addEventListener('ended', () => { console.log('ended') });
+    // const devices = await getConnectedDevices(); // ?????
 
-    setTimeout(async () => {
-        console.log('applyConstraints');
-        await mediaStream.getVideoTracks()[0].applyConstraints({ width: 640, height: 360 });
-    }, 1000)
+    // async function getConnectedDevices() {
+    //     const devices = await navigator.mediaDevices.enumerateDevices();
+    //     return devices;
+    // }
+
+    const configuration = {};
+    const peerConnection1 = new RTCPeerConnection(configuration);
+    const peerConnection2 = new RTCPeerConnection(configuration);
+
+    localStream.getTracks().forEach(track => peerConnection1.addTrack(track, localStream));
+    console.log('peerConnection1 tracks added', peerConnection1);
+
+    const offerOptions = {
+        offerToReceiveAudio: 1,
+        offerToReceiveVideo: 1
+    };
+    const offer = await peerConnection1.createOffer(offerOptions);
+    console.log('offer sdp', offer.sdp);
+
+    await peerConnection1.setLocalDescription(offer);
+    await peerConnection2.setRemoteDescription(offer);
+
+    const answer = await peerConnection2.createAnswer();
+    console.log('answer sdp', answer.sdp);
+
+    await peerConnection2.setLocalDescription(answer);
+    await peerConnection1.setRemoteDescription(answer);
 })()
